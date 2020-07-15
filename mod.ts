68e27748d64fc85a848d46caabaef3e7c8d3c0c0
Ali Hasani <a.hassssani@gmail.com>
2020-07-15 12:03:14 +0430

    (api) make prefix, suffix and bold global configurable (#16)

--- mod.ts
+++ mod.ts
@@ -1,20 +1,37 @@
 import { InteractOptions, InteractType } from "./src/core/interact.ts";
 export { InteractType };
 import { Any, Numeric, Choice } from "./src/types/mod.ts";
+import { deepExtend } from "./src/helpers/deepExtend.ts";
+
+type globalOptions = {
+  prefix?: string;
+  suffix?: string;
+  bold?: boolean;
+};
 
 export class Interaction {
   answers: Record<string, string | number> = {};
+  globalOptions: globalOptions;
+  constructor(globalOptions?: globalOptions) {
+    this.globalOptions = globalOptions || {};
+  }
+
   async interact(opts: Array<InteractOptions>): Promise<any> {
     for (const opt of opts) {
+      const mergedOptions = deepExtend(
+        { ...this.globalOptions },
+        opt,
+      ) as InteractOptions;
+
       switch (opt.type) {
         case InteractType.any:
-          this.answers[opt.name] = await new Any(opt).request();
+          this.answers[opt.name] = await new Any(mergedOptions).request();
           break;
         case InteractType.numeric:
-          this.answers[opt.name] = await new Numeric(opt).request();
+          this.answers[opt.name] = await new Numeric(mergedOptions).request();
           break;
         case InteractType.choice:
-          this.answers[opt.name] = await new Choice(opt).request();
+          this.answers[opt.name] = await new Choice(mergedOptions).request();
           break;
       }
     }
